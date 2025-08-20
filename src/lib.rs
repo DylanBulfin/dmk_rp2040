8853812b5fbb399f3c78c8a54037154f1fed0cc7
Dylan Bulfin <dylanbulfin@gmail.com>
2025-08-20 12:13:45 -0400

    Fix rest of issues

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,21 @@ use dmk::{
     scanning::ScanAlgorithm,
     state::State,
     timer::{Duration, Timer},
+    vboard::VirtualKeyboard,
 };
+// Need to import the embedded-hal 0.2 CountDown trait, this is the best way I know
+use cortex_m::prelude::_embedded_hal_timer_CountDown as _;
 use embedded_hal::digital::{InputPin, OutputPin};
 use frunk::{HCons, HNil};
+use fugit::ExtU32;
 #[cfg(feature = "active_low")]
 use rp2040_hal::gpio::PullUp;
 #[cfg(feature = "active_high")]
-use rp2040_hal::gpio::{FunctionSio, SioOutput};
+use rp2040_hal::gpio::{FunctionSio, FunctionSioOutput, SioOutput};
 use rp2040_hal::{
     Sio, Watchdog,
     gpio::{
-        DynPinId, FunctionNull, Pin, Pins, PullDown,
+        DynPinId, FunctionNull, FunctionSioInput, Pin, Pins, PullDown,
         bank0::{
             Gpio0, Gpio1, Gpio2, Gpio3, Gpio4, Gpio5, Gpio6, Gpio7, Gpio8, Gpio9, Gpio10, Gpio11,
             Gpio12, Gpio13, Gpio14, Gpio15, Gpio16, Gpio17, Gpio18, Gpio19, Gpio20, Gpio21, Gpio22,
@@ -43,9 +47,9 @@ use usbd_human_interface_device::{
 pub struct PinCollection {
     active_high: bool,
     pins: MaybePins,
-    input_pins: [Option<Pin<DynPinId, FunctionNull, PullDown>>; 30],
+    input_pins: [Option<Pin<DynPinId, FunctionSioInput, PullDown>>; 30],
     #[cfg(feature = "active_high")]
-    output_pins: [Option<Pin<DynPinId, FunctionSio<SioOutput>, PullDown>>; 30],
+    output_pins: [Option<Pin<DynPinId, FunctionSioOutput, PullDown>>; 30],
     #[cfg(feature = "active_low")]
     output_pins: [Option<Pin<DynPinId, FunctionSio<SioOutput>, PullDown>>; 30],
 }
@@ -123,36 +127,186 @@ impl From<Pins> for MaybePins {
 macro_rules! match_pin {
     (input: $pin:ident, $pins:expr) => {
         match $pin {
-            0 => $pins.gpio0.take().expect("Missing pin").into_dyn_pin(),
-            1 => $pins.gpio1.take().expect("Missing pin").into_dyn_pin(),
-            2 => $pins.gpio2.take().expect("Missing pin").into_dyn_pin(),
-            3 => $pins.gpio3.take().expect("Missing pin").into_dyn_pin(),
-            4 => $pins.gpio4.take().expect("Missing pin").into_dyn_pin(),
-            5 => $pins.gpio5.take().expect("Missing pin").into_dyn_pin(),
-            6 => $pins.gpio6.take().expect("Missing pin").into_dyn_pin(),
-            7 => $pins.gpio7.take().expect("Missing pin").into_dyn_pin(),
-            8 => $pins.gpio8.take().expect("Missing pin").into_dyn_pin(),
-            9 => $pins.gpio9.take().expect("Missing pin").into_dyn_pin(),
-            10 => $pins.gpio10.take().expect("Missing pin").into_dyn_pin(),
-            11 => $pins.gpio11.take().expect("Missing pin").into_dyn_pin(),
-            12 => $pins.gpio12.take().expect("Missing pin").into_dyn_pin(),
-            13 => $pins.gpio13.take().expect("Missing pin").into_dyn_pin(),
-            14 => $pins.gpio14.take().expect("Missing pin").into_dyn_pin(),
-            15 => $pins.gpio15.take().expect("Missing pin").into_dyn_pin(),
-            16 => $pins.gpio16.take().expect("Missing pin").into_dyn_pin(),
-            17 => $pins.gpio17.take().expect("Missing pin").into_dyn_pin(),
-            18 => $pins.gpio18.take().expect("Missing pin").into_dyn_pin(),
-            19 => $pins.gpio19.take().expect("Missing pin").into_dyn_pin(),
-            20 => $pins.gpio20.take().expect("Missing pin").into_dyn_pin(),
-            21 => $pins.gpio21.take().expect("Missing pin").into_dyn_pin(),
-            22 => $pins.gpio22.take().expect("Missing pin").into_dyn_pin(),
-            23 => $pins.gpio23.take().expect("Missing pin").into_dyn_pin(),
-            24 => $pins.gpio24.take().expect("Missing pin").into_dyn_pin(),
-            25 => $pins.gpio25.take().expect("Missing pin").into_dyn_pin(),
-            26 => $pins.gpio26.take().expect("Missing pin").into_dyn_pin(),
-            27 => $pins.gpio27.take().expect("Missing pin").into_dyn_pin(),
-            28 => $pins.gpio28.take().expect("Missing pin").into_dyn_pin(),
-            29 => $pins.gpio29.take().expect("Missing pin").into_dyn_pin(),
+            0 => $pins
+                .gpio0
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            1 => $pins
+                .gpio1
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            2 => $pins
+                .gpio2
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            3 => $pins
+                .gpio3
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            4 => $pins
+                .gpio4
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            5 => $pins
+                .gpio5
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            6 => $pins
+                .gpio6
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            7 => $pins
+                .gpio7
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            8 => $pins
+                .gpio8
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            9 => $pins
+                .gpio9
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            10 => $pins
+                .gpio10
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            11 => $pins
+                .gpio11
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            12 => $pins
+                .gpio12
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            13 => $pins
+                .gpio13
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            14 => $pins
+                .gpio14
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            15 => $pins
+                .gpio15
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            16 => $pins
+                .gpio16
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            17 => $pins
+                .gpio17
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            18 => $pins
+                .gpio18
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            19 => $pins
+                .gpio19
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            20 => $pins
+                .gpio20
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            21 => $pins
+                .gpio21
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            22 => $pins
+                .gpio22
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            23 => $pins
+                .gpio23
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            24 => $pins
+                .gpio24
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            25 => $pins
+                .gpio25
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            26 => $pins
+                .gpio26
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            27 => $pins
+                .gpio27
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            28 => $pins
+                .gpio28
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
+            29 => $pins
+                .gpio29
+                .take()
+                .expect("Missing pin")
+                .into_pull_down_input()
+                .into_dyn_pin(),
             _ => panic!("Nonexistent pin"),
         }
     };
@@ -370,22 +524,22 @@ impl PinSet for PinCollection {
     }
 
     fn set_pin_output(&mut self, pin: usize) {
-        self.output_pins[pin] = Some(match_pin!(output: pin, self.pins));
+        let mut p = match_pin!(output: pin, self.pins);
+        p.set_output_disable(false);
+        self.output_pins[pin] = Some(p);
     }
 
-    fn get_pin_state(&self, pin: usize) -> bool {
+    fn get_pin_state(&mut self, pin: usize) -> bool {
         if self.active_high {
             self.input_pins[pin]
-                .as_ref()
+                .as_mut()
                 .expect("Missing pin")
-                .as_input()
                 .is_high()
                 .expect("Error reading input pin")
         } else {
             self.input_pins[pin]
-                .as_ref()
+                .as_mut()
                 .expect("Missing pin")
-                .as_input()
                 .is_low()
                 .expect("Error reading input pin")
         }
@@ -404,7 +558,7 @@ impl PinSet for PinCollection {
                 .expect("Missing pin")
                 .set_low()
                 .expect("Error writing output pin");
-        }
+        };
     }
 }
 
@@ -467,7 +621,8 @@ where
             &mut pac.RESETS,
         ));
 
-        let pin_collection = PinCollection::init(pins, true);
+        let mut pin_collection = PinCollection::init(pins, true);
+        algorithm.configure_pins(&mut pin_collection);
 
         let layout = ControllerLayout::new(keys, pin_collection, algorithm);
 
@@ -480,29 +635,7 @@ where
     }
 }
 
-fn create_usb_extras(
-    usb_bus: &UsbBusAllocator<UsbBus>,
-) -> (
-    UsbHidClass<'_, UsbBus, HCons<NKROBootKeyboard<'_, UsbBus>, HNil>>,
-    UsbDevice<'_, UsbBus>,
-) {
-    (
-        UsbHidClassBuilder::new()
-            .add_device(
-                usbd_human_interface_device::device::keyboard::NKROBootKeyboardConfig::default(),
-            )
-            .build(usb_bus),
-        UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x1209, 0x0001))
-            .strings(&[StringDescriptors::default()
-                .manufacturer("Dylan Bulfin")
-                .product("Boot keyboard")
-                .serial_number("TEST")])
-            .unwrap()
-            .build(),
-    )
-}
-
-pub fn main_loop<A, C>(rp_state: RP2040BaseState<A>, layers: C) -> !
+pub fn main_loop<A, C>(xtal_freq_hz: u32, keys: usize, algorithm: A, layers: C) -> !
 where
     A: ScanAlgorithm,
     C: Index<usize, Output = Layer>,
@@ -512,16 +645,28 @@ where
         usb_bus,
         layout,
         watchdog,
-    } = rp_state;
-    let (mut keyboard, mut usb_device) = create_usb_extras(&usb_bus);
+    } = RP2040BaseState::new(xtal_freq_hz, keys, algorithm);
 
-    // let mut kb_state = State::new(layers, layout, timer);
+    let mut keyboard = UsbHidClassBuilder::new()
+        .add_device(
+            usbd_human_interface_device::device::keyboard::NKROBootKeyboardConfig::default(),
+        )
+        .build(&usb_bus);
+    let mut usb_device = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x1209, 0x0001))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("Dylan Bulfin")
+            .product("Boot keyboard")
+            .serial_number("TEST")])
+        .unwrap()
+        .build();
 
-    let mut next_tick = timer.as_instant();
-    let mut next_scan = timer.as_instant();
+    let mut kb_state = State::new(layers, layout, timer);
+
+    let mut next_tick = kb_state.timer_state.timer.as_instant();
+    let mut next_scan = kb_state.timer_state.timer.as_instant();
 
     loop {
-        if next_tick <= timer.as_instant() {
+        if next_tick <= kb_state.timer_state.timer.as_instant() {
             match keyboard.tick() {
                 Err(UsbHidError::WouldBlock) | Ok(_) => {}
                 Err(e) => core::panic!("Failed to process keyboard tick: {:?}", e),
@@ -529,17 +674,21 @@ where
 
             watchdog.feed();
 
-            next_tick = timer.add_duration(Duration::from_millis(1));
+            next_tick = kb_state
+                .timer_state
+                .timer
+                .add_duration(Duration::from_millis(1));
         }
 
         if usb_device.poll(&mut [&mut keyboard]) {
             keyboard.device().read_report();
         }
 
-        if next_scan <= timer.as_instant() {
-            let keys = [Keyboard::Z; 1];
+        if next_scan <= kb_state.timer_state.timer.as_instant() {
+            // let keys = [Keyboard::Z; 1];
+            let keys_full = convert_vboard(kb_state.get_vboard());
 
-            match keyboard.device().write_report(keys) {
+            match keyboard.device().write_report(keys_full) {
                 Err(UsbHidError::WouldBlock) => {}
                 Err(UsbHidError::Duplicate) => {}
                 Ok(_) => {}
@@ -548,11 +697,110 @@ where
                 }
             }
 
-            next_scan = timer.add_duration(Duration::from_millis(10));
+            next_scan = kb_state
+                .timer_state
+                .timer
+                .add_duration(Duration::from_millis(10));
         }
 
-        // kb_state.main_iteration();
+        kb_state.main_iteration();
     }
+}
+
+fn convert_vboard(vboard: &VirtualKeyboard) -> [Keyboard; 6] {
+    let mut keys = [Keyboard::NoEventIndicated; 6];
+    let mut key = 0;
+
+    let mut add_elem = |event: Keyboard| {
+        if key <= 5 {
+            keys[key] = event;
+            key += 1;
+        }
+    };
+
+    if vboard.keys.a {
+        add_elem(Keyboard::A);
+    }
+    if vboard.keys.b {
+        add_elem(Keyboard::B);
+    }
+    if vboard.keys.c {
+        add_elem(Keyboard::C);
+    }
+    if vboard.keys.d {
+        add_elem(Keyboard::D);
+    }
+    if vboard.keys.e {
+        add_elem(Keyboard::E);
+    }
+    if vboard.keys.f {
+        add_elem(Keyboard::F);
+    }
+    if vboard.keys.g {
+        add_elem(Keyboard::G);
+    }
+    if vboard.keys.h {
+        add_elem(Keyboard::H);
+    }
+    if vboard.keys.i {
+        add_elem(Keyboard::I);
+    }
+    if vboard.keys.j {
+        add_elem(Keyboard::J);
+    }
+    if vboard.keys.k {
+        add_elem(Keyboard::K);
+    }
+    if vboard.keys.l {
+        add_elem(Keyboard::L);
+    }
+    if vboard.keys.m {
+        add_elem(Keyboard::M);
+    }
+    if vboard.keys.n {
+        add_elem(Keyboard::N);
+    }
+    if vboard.keys.o {
+        add_elem(Keyboard::O);
+    }
+    if vboard.keys.p {
+        add_elem(Keyboard::P);
+    }
+    if vboard.keys.q {
+        add_elem(Keyboard::Q);
+    }
+    if vboard.keys.r {
+        add_elem(Keyboard::R);
+    }
+    if vboard.keys.s {
+        add_elem(Keyboard::S);
+    }
+    if vboard.keys.t {
+        add_elem(Keyboard::T);
+    }
+    if vboard.keys.u {
+        add_elem(Keyboard::U);
+    }
+    if vboard.keys.v {
+        add_elem(Keyboard::V);
+    }
+    if vboard.keys.w {
+        add_elem(Keyboard::W);
+    }
+    if vboard.keys.x {
+        add_elem(Keyboard::X);
+    }
+    if vboard.keys.y {
+        add_elem(Keyboard::Y);
+    }
+    if vboard.keys.z {
+        add_elem(Keyboard::Z);
+    }
+    if vboard.keys.lsft {
+        add_elem(Keyboard::LeftShift);
+    }
+
+    keys
 }
 
 #[cfg(test)]
